092f2426feff9bd4460f9d007d151500291a5adf
Mikey Gee <mikey@mikeygee.com>
2013-06-22 00:52:40 -0700

    new lyrics scrape with node

--- lyrics_scrape.js
+++ lyrics_scrape.js
@@ -1,9 +1,55 @@
-// lyric wiki jquery scraper [http://lyrics.wikia.com]
+var artist = process.argv[2] && process.argv[2].replace(/\s+/g, "_");
+var track = process.argv[3] && process.argv[3].replace(/\s+/g, "_");
+if(!artist || !track) {
+	console.log("Usage: node lyrics_scrape.js [artist] [track]");
+	process.exit(1);
+}
 
-var song = {};
-var header = $("#WikiaPageHeader h1").html().split(":");
-song.artist = header[0];
-song.title = header[1];
-var lyricsDiv = $(".lyricbox").html().replace(/<i>|<\/i>/g,"");
-song.lyrics = lyricsDiv.substring(lyricsDiv.indexOf("</div>")+6, lyricsDiv.indexOf("<!")).split("<br>");
+var url = "http://lyrics.wikia.com/" + artist + ":" + track;
+var request = require("request");
+var cheerio = require("cheerio");
 
+var start = new Date();
+console.log("Fetching lyrics from " + url);
+request(url, function(err, response, html) {
+	if(err) return console.error(err);
+	var dl = new Date();
+	console.log("Fetch completed in " + (dl-start) + " ms");
+	var $ = cheerio.load(html);
+	$("div.lyricbox > .rtMatcher, div.lyricbox > .lyricsbreak").remove();
+	$("div.lyricbox > br").replaceWith("\n");
+	var lyrics = $("div.lyricbox").text();
+	var done = new Date();
+	console.log("Lyrics scrape completed in " + (done-start) + " ms");
+	console.log(lyrics.split("\n"));
+	process.exit(0);
+});
+/*
+var jsdom = require("jsdom");
+var start = new Date();
+console.log("Fetching lyrics from " + url);
+jsdom.env( url, ["http://code.jquery.com/jquery.min.js"],
+	function (errors, window) {
+		var dl = new Date();
+		console.log("Fetch completed in " + (dl-start) + " ms");
+		var lyrics = window.$("div.lyricbox").contents()
+			.filter(function() {
+				return (this.nodeType == 3 && this.nodeValue.search(/^\s+$/) < 0) || (this.nodeName.toLowerCase() == "br");
+			})
+			.map(function() {
+				if(this.nodeType == 3)
+					return this.nodeValue;
+				else
+					return "";
+			})
+			.toArray()
+			.filter(function(val, i, arr) {
+				return val !== "" || arr[i+1] === "";
+			});
+		console.log(lyrics);
+		var done = new Date();
+		console.log("Lyrics scrape completed in " + (done-start) + " ms");
+		process.exit(0);
+	}
+);
+*/
